f3c81b1637fdfd44919793b15d8878dd888bb576
zhanghao <767802022@qq.com>
2020-11-16 17:38:22 +0800

    新增gis数据入库效率测试，压缩，加密，下载等样例程序

---
+++ CompressSample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace CompressSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestGZipCompressFile();
+
+            string str = "abssssdddssssssssss11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
+            string orgStr = str;
+            string result = "";
+
+            //将传入的字符串直接进行压缩，解压缩，会出现乱码。
+            //先转码，然后再压缩。解压缩：先解压缩，再反转码
+            Console.WriteLine("源字符串为：{0}", str);            
+            result = GZipCompress.Compress(str);
+            Console.WriteLine("压缩后为：{0}", result);
+
+            Console.Write("压缩前：{0},压缩后：{1}", str.Length, result.Length);
+
+            Console.WriteLine("开始解压...");
+            Console.WriteLine("解压后：{0}", result);
+            result = GZipCompress.Decompress(result);
+            Console.WriteLine("解压后与源字符串对比，是否相等：{0}", result == orgStr);
+
+
+            Console.WriteLine("源字符串为：{0}", str);
+            result = ZipComporessor.Compress(str);
+            Console.WriteLine("压缩后为：{0}", result);
+
+            Console.Write("压缩前：{0},压缩后：{1}", str.Length, result.Length);
+
+            Console.WriteLine("开始解压...");
+            Console.WriteLine("解压后：{0}", result);
+            result = ZipComporessor.Decompress(result);
+            Console.WriteLine("解压后与源字符串对比，是否相等：{0}", result == orgStr);
+
+            Console.WriteLine("输入任意键，退出！");
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 测试GZipCompress压缩文件
+        /// </summary>
+        public static void TestGZipCompressFile()
+        {
+            string filePath = @"c:\test.txt";
+
+            FileInfo fileToCompress = new FileInfo(filePath);
+            GZipCompress.Compress(fileToCompress);
+
+            FileInfo fileToDecompress = new FileInfo(@"c:\test.txt.gz");
+            GZipCompress.Decompress(fileToDecompress);
+        }
+    }
+}
---
+++ EncriptSample/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace EncriptSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "abc";
+            input = "银行密码统统都给我";
+            string key = "justdoit";
+            string result = string.Empty;
+            result = Encrypter.EncryptByMD5(input);
+            Console.WriteLine("MD5加密结果：{0}",result);
+
+            result = Encrypter.EncryptBySHA1(input);
+            Console.WriteLine("SHA1加密结果：{0}", result);
+
+            result = Encrypter.EncryptString(input, key);
+            Console.WriteLine("DES加密结果：{0}", result);
+
+
+            result = Encrypter.DecryptString(result, key);
+            Console.WriteLine("DES解密结果：{0}", result);
+
+            result = Encrypter.EncryptByDES(input,key);
+            Console.WriteLine("DES加密结果：{0}", result);
+            
+
+            result = Encrypter.DecryptByDES(result, key);
+            Console.WriteLine("DES解密结果：{0}", result); //结果："银行密码统统都给我�\nJn7"，与明文不一致，为什么呢？在加密后，通过base64编码转为字符串，可能是这个问题。
+
+            key = "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
+
+            result = Encrypter.EncryptByAES(input, key);
+            Console.WriteLine("AES加密结果：{0}", result);
+
+            result = Encrypter.DecryptByAES(result, key);
+            Console.WriteLine("AES解密结果：{0}", result);
+
+
+            KeyValuePair<string,string> keyPair = Encrypter.CreateRSAKey();
+            string privateKey = keyPair.Value;
+            string publicKey = keyPair.Key;
+
+            result = Encrypter.EncryptByRSA(input, publicKey);
+            Console.WriteLine("RSA私钥加密后的结果：{0}",result);
+
+            result = Encrypter.DecryptByRSA(result, privateKey);
+            Console.WriteLine("RSA公钥解密后的结果：{0}", result);
+
+            //密钥加密，公钥解密
+            result = Encrypter.EncryptByRSA(input, privateKey);
+            Console.WriteLine("RSA私钥加密后的结果：{0}", result);
+
+            result = Encrypter.DecryptByRSA(result, publicKey);
+            Console.WriteLine("RSA公钥解密后的结果：{0}", result);
+
+            TestSign();
+
+            Console.WriteLine("输入任意键退出！");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 测试数字签名
+        /// </summary>
+        public static void TestSign()
+        {
+            string originalData = "文章不错，这是我的签名：奥巴马！";
+            Console.WriteLine("签名数为：{0}",originalData);
+            KeyValuePair<string,string> keyPair = Encrypter.CreateRSAKey();
+            string privateKey = keyPair.Value;
+            string publicKey = keyPair.Key;
+
+            //1、生成签名，通过摘要算法
+            string signedData = Encrypter.HashAndSignString(originalData, privateKey);
+            Console.WriteLine("数字签名:{0}",signedData);
+
+            //2、验证签名
+            bool verify = Encrypter.VerifySigned(originalData, signedData,publicKey);
+            Console.WriteLine("签名验证结果：{0}",verify);
+        }
+    }
+}
